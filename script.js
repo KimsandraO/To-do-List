2f09e9ceff810bd0000f751e68bc51ad74c20976
David Niermann <dc.niermann@gmail.com>
2023-01-19 09:40:01 +0100

    input form reset

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 const items = document.getElementById("items");
 let toDos = [];
 
-const addToDo = (event) => {
+const addToDo = (e) => {
+  e.preventDefault();
   toDos.push(document.getElementById("toDoInput").value);
   let curIndex = toDos.length - 1;
   let createDiv = document.createElement("div");
   createDiv.classList.add("row");
   createDiv.innerHTML = `<div class="col"><input type="checkbox" id="cbitem${curIndex}">
   <input type="text" id="item${curIndex}" value="${toDos.at(curIndex)}"></div>`;
-  items.appendChild(createDiv);
-  event.preventDefault();
+  itemForm.appendChild(createDiv);
+  toDoInputForm.reset();
 };
 addToDoBtn.addEventListener("click", addToDo);
