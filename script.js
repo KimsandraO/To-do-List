97d05375fc98613b523f8a650b78a41eb99af8ab
kevin ceka <kevinkceka@gmail.com>
2023-01-23 17:42:59 +0100

    my stuff

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const addToDoBtn = document.getElementById("add-todo-btn");
 const items = document.getElementById("items");
+const toDoInput=    document.getElementById("toDoInput");
+let nextItem = items.children.length;
 
 const addToDo = (event) => {
-  let createDiv = document.createElement("div");
-  createDiv.classList.add("row");
-  createDiv.innerHTML =
-    '<div class="col"><input type="checkbox"><label for="item1"> ' +
-    document.getElementById("toDoInput").value +
-    "</label></div>";
-  items.appendChild(createDiv);
+  nextItem= nextItem+1;
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("row");
+  newDiv.innerHTML =
+  `<div class="col">
+        <input type="checkbox" id="item${nextItem}">
+        <label for="item${nextItem}">
+        <div class="row">
+        ${toDoInput.value}
+        </div>
+        </label>
+        </div>`;
+  items.appendChild(newDiv);
   event.preventDefault();
 };
 
