b5edd9177cf0d128cec67e0a4110fb0930618f26
David Niermann <dc.niermann@gmail.com>
2023-01-23 19:38:29 +0100

    Local storage capacity

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
-toDoInputForm.reset();
+toDoInputForm.reset(); // Reset input form when loading DOM
+let toDos = [];
 
 // Add functionality
 // const items = document.getElementById("items");
 const toDoInput = document.getElementById("toDoInput");
-let toDos = [];
+
+// Read from local storage and fill array with toDos[]
+const storedItems = localStorage.getItem("toDos");
+if (storedItems) {
+  toDos = JSON.parse(storedItems);
+  for (i = 0; i < toDos.length; i++) {
+    if (toDos[i]) {
+      createToDoItems(i);
+    }
+  }
+}
+
+// Function that creates ToDos
+function createToDoItems(curIndex) {
+  if (!curIndex) {
+    const curIndex = toDos.length - 1;
+  }
+  const liEle = document.createElement("li"); // Create li element
+  liEle.id = `liItem${curIndex}`; // Add an id with $curIndex to li element
+  liEle.innerHTML = `<input type="checkbox" id="cbItem${curIndex}">
+  <input type="text" id="inputItem${curIndex}" class="inputItem" value="${toDos.at(
+    curIndex
+  )}">`; // Fill innerHTML of li element with checkbox and content (input field)
+
+  // Delete item event listener on click of Delete button
+  const delBtn = document.createElement("button"); //Create delete button
+  delBtn.textContent = "X"; //
+  delBtn.addEventListener("click", (e) => {
+    itemList.removeChild(liEle); // Remove li ToDo item from DOM
+    delete toDos[curIndex]; // Delete from toDos[] array at index $curIndex
+    storeLocal(); // Delete from local storage
+  });
+
+  //
+  liEle.appendChild(delBtn);
+  itemList.appendChild(liEle);
+
+  // Edit item event listener on blur (focus lost) of input field
+  const inputItem = document.getElementById("inputItem" + curIndex);
+  inputItem.addEventListener("blur", (e) => {
+    toDos[curIndex] = inputItem.value; // Change toDos[] array at index $curIndex with edit
+    storeLocal(); // Save to local storage after edit
+  });
+}
+
+// Function that saves to local storage
+function storeLocal() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
 
 const addToDo = (e) => {
   e.preventDefault();
   if (toDoInput.value) {
-    toDos.push(toDoInput.value);
-    const curIndex = toDos.length - 1;
-    const liEle = document.createElement("li");
-    liEle.id = `liItem${curIndex}`;
-    liEle.innerHTML = `<input type="checkbox" id="cbItem${curIndex}">
-  <input type="text" id="inputItem${curIndex}" class="inputItem" value="${toDos.at(
-      curIndex
-    )}">`;
-    const delBtn = document.createElement("button");
-    delBtn.textContent = "X";
-    delBtn.addEventListener("click", (e) => {
-      itemList.removeChild(liEle);
-      delete toDos[curIndex];
-      console.log(toDos);
-    });
-    const editHidden = document.createElement("input");
-    editHidden.setAttribute("type", "hidden");
-    liEle.appendChild(delBtn);
-    liEle.appendChild(editHidden);
-    itemList.appendChild(liEle);
-    const inputItem = document.getElementsByClassName("inputItem")[curIndex];
-    inputItem.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        toDos[curIndex] = inputItem.value;
-        console.log("edit works!" + curIndex);
-      }
-    });
-    toDoInputForm.reset();
-    console.log(toDos);
+    toDos.push(toDoInput.value); // Push value from input field to toDos[] array
+    createToDoItems(toDos.length - 1);
+    storeLocal(); // Save to local storage after add
+    toDoInputForm.reset(); // Reset input form
   } else {
-    alert("Don't try adding empty ToDos, you lazy bastard!");
+    alert("Don't try adding empty ToDos, you lazy bastard!"); // Alert when ToDo input field is empty
   }
 };
 addToDoBtn.addEventListener("click", addToDo);
